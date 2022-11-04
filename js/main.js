e1f9fcd4bc72699f13428451708c867064d43031
robpap96 <robertopapagni1996@outlook.it>
2022-11-04 14:55:18 +0100

    fatto

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+
+
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
 "use strict";
-const numbers = [];
+
+const numberContainer = document.querySelector(".container");
+
+const Numbers = [];
 function creaRndNum(str, range){
     for(let i=0; i<5; i++) {
         let num = parseInt(Math.random()*range + 1);
@@ -10,9 +18,40 @@ function creaRndNum(str, range){
         }
         str.push(num);
     }
+    //deb
+    return str;
 }
-creaRndNum(numbers, 100);
-console.log(numbers);
+const newS = creaRndNum(Numbers, 100);
+const number = document.createElement("div");
+number.innerHTML = newS;
+numberContainer.append(number);
 
-const divNum = document.querySelector("main");
-divNum.document.createElement()
+const guessedNumber = document.createElement("h1");
+const userNum =[];
+const indovinati = [];
+let cnt = 0;
+
+
+setTimeout(function(){
+    numberContainer.classList.add("hide-item");
+}, 3000);
+
+setTimeout(function(){
+
+    for(let i=0; i<5; i++) {
+        userNum[i] = Number(prompt(`Ora inserisci i numeri uno alla volta`));
+    }
+    
+    for(let i=0; i<5; i++){
+        for(let j=0; j<5; j++){
+            if(newS[i] === userNum[j]){
+                cnt++;
+                indovinati.push(userNum[i]);
+            }
+        }
+    }
+    numberContainer.classList.remove("hide-item");
+    guessedNumber.innerHTML =`<br> Bravo!! hai indovinato ${cnt} numeri, e sono ->${indovinati}`;
+    numberContainer.append(guessedNumber);
+},4000);
+
